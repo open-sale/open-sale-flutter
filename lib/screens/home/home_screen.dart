3b1acc7d6ab1516a4c6d6c3f19696d4e931853e3
muathye <muath.ye@gmail.com>
2022-01-14 00:11:53 +0300

    Update home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'components/build_app_bar.dart';
 import '../../constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
   }
+}
