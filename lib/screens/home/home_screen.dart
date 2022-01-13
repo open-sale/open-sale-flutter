4512fed6a242937fbd383678215c0f7a4010003c
muathye <muath.ye@gmail.com>
2022-01-14 00:06:34 +0300

    Add home_screen.dart

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
