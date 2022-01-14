01ffcfd541fb24017f59223bf9a6d92988216df2
muathye <muath.ye@gmail.com>
2022-01-14 09:02:55 +0300

    Add item_card.dart

---
+++ lib/screens/home/components/item_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../../../models/Product.dart';
+
+import '../../../constants.dart';
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback press;
+  const ItemCard({
+    required this.product,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(kDefaultPaddin),
+              // For  demo we use fixed height  and width
+              // Now we dont need them
+              // height: 180,
+              // width: 160,
+              decoration: BoxDecoration(
+                color: product.color,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Hero(
+                tag: "${product.id}",
+                child: Image.asset(product.image),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+            child: Text(
+              // products is out demo list
+              product.title,
+              style: const TextStyle(color: kTextLightColor),
+            ),
+          ),
+          Text(
+            "\$${product.price}",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+}
