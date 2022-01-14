c557d1cc62cfec619faef28519bf6f35fea509e8
muathye <muath.ye@gmail.com>
2022-01-14 09:02:36 +0300

    Add body.dart

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+import '../../../models/Product.dart';
+import '../../details/details_screen.dart';
+import 'categorries.dart';
+import 'item_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                      product: products[index],
+                      press: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsScreen(
+                            product: products[index],
+                          ),
+                        ),
+                      ),
+                      // key: null,
+                    )),
+          ),
+        ),
+      ],
+    );
+  }
+}
