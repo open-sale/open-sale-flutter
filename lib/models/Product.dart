609dc5d912b27ac890987f8be23523a68db7f411
muathye <muath.ye@gmail.com>
2022-01-16 14:47:19 +0300

    Update product model

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,73 +1,55 @@
-// ignore: file_names
-import 'package:flutter/material.dart';
+/// To parse this JSON data, do
+///
+///     final product = productFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Product> productFromJson(String str) =>
+    List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Product {
-  final String image, title, description;
-  final int price, size, id;
-  final Color color;
   Product({
     required this.id,
-    required this.image,
-    required this.title,
-    required this.price,
+    required this.name,
     required this.description,
+    required this.image,
+    required this.price,
     required this.size,
     required this.color,
+    required this.category,
   });
+
+  int id;
+  String name;
+  String description;
+  String image;
+  String price;
+  String size;
+  String color;
+  String category;
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        image: json["image"],
+        price: json["price"],
+        size: json["size"],
+        color: json["color"],
+        category: json["category"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "image": image,
+        "price": price,
+        "size": size,
+        "color": color,
+        "category": category,
+      };
 }
-
-
-String dummyText =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. When an unknown printer took a galley.";
-
-List<Product> products = [
-  Product(
-      id: 1,
-      title: "Office Code",
-      price: 234,
-      size: 12,
-      description: dummyText,
-      image: "assets/images/bag_1.png",
-      color: Color(0xFF3D82AE)),
-  Product(
-      id: 2,
-      title: "Belt Bag",
-      price: 234,
-      size: 8,
-      description: dummyText,
-      image: "assets/images/bag_2.png",
-      color: Color(0xFFD3A984)),
-  Product(
-      id: 3,
-      title: "Hang Top",
-      price: 234,
-      size: 10,
-      description: dummyText,
-      image: "assets/images/bag_3.png",
-      color: Color(0xFF989493)),
-  Product(
-      id: 4,
-      title: "Old Fashion",
-      price: 234,
-      size: 11,
-      description: dummyText,
-      image: "assets/images/bag_4.png",
-      color: Color(0xFFE6B398)),
-  Product(
-      id: 5,
-      title: "Office Code",
-      price: 234,
-      size: 12,
-      description: dummyText,
-      image: "assets/images/bag_5.png",
-      color: Color(0xFFFB7883)),
-  Product(
-    id: 6,
-    title: "Office Code",
-    price: 234,
-    size: 12,
-    description: dummyText,
-    image: "assets/images/bag_6.png",
-    color: Color(0xFFAEAEAE),
-  ),
-];
