960b155e16476d90d8cf1648b37943adfeec590d
muathye <muath.ye@gmail.com>
2022-01-16 14:47:31 +0300

    Add category model

---
+++ lib/models/category.dart
@@ -0,0 +1,27 @@
+// To parse this JSON data, do
+//
+//     final category = categoryFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Category> categoryFromJson(String str) =>
+    List<Category>.from(json.decode(str).map((x) => Category.fromJson(x)));
+
+String categoryToJson(List<Category> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Category {
+  Category({
+    required this.name,
+  });
+
+  String name;
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+      };
+}
