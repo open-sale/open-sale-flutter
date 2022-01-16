a31ffb9a029e824047995fe8b03f1ac3ffc80f86
muathye <muath.ye@gmail.com>
2022-01-16 14:55:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:laracommerce/screens/home/home_screen.dart';
 import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Open Sale',
       theme: ThemeData(
@@ -20,7 +21,14 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      // home: HomeScreen(),
+      // init route
+      initialRoute: '/HomeScreen',
+      // Instead of the classic 'routes' we use the GetX routing management,
+      // avoiding the context parameter as mentioned above
+      getPages: [
+           GetPage(name: '/HomeScreen', page: () => const HomeScreen()),
+      ],
     );
   }
 }
